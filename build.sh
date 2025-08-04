7dc237fc1a5c8fb96e448b3529057b744a087d7c
mehdimsss <146398726+mehdimsss@users.noreply.github.com>
2025-08-04 10:01:56 +0200

    Fix-Remove-apt-get-update-from-build.sh-for-Render-compatibility

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 set -o errexit
 
 # Installer les paquets systeme
-apt-get update
-apt-get install -y tesseract-ocr tesseract-ocr-fra
+
 
 # Installer les dépendances Python
 pip install -r requirements.txt
